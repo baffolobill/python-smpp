64baf3350a90eebffc94fa05272f3ca517ff76da
Jeremy Thurgood <jeremy@praekelt.com>
2014-10-15 12:52:20 +0200

    Bump version post-release.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ def listify(filename):
 
 setup(
     name="python-smpp",
-    version="0.1.4",
+    version="0.1.5a",
     url='http://github.com/praekelt/python-smpp',
     license='BSD',
     description="Python SMPP Library",
