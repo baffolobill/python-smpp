e0bbd05c252438d157de6f9b85079848920f574e
Rudi Giesler <rudi@praekelt.com>
2015-08-04 10:36:05 +0200

    Use absolute path for reading description from file (thanks @hodgestar)

--- setup.py
+++ setup.py
@@ -1,8 +1,14 @@
+import os
 from setuptools import setup, find_packages
 
 
 def listify(filename):
     return filter(None, open(filename, 'r').readlines())
+
+
+def read_file(filename):
+    filepath = os.path.join(os.path.dirname(__file__), filename)
+    return open(filepath, 'r').read()
 
 setup(
     name="python-smpp",
@@ -10,7 +16,7 @@ setup(
     url='http://github.com/praekelt/python-smpp',
     license='BSD',
     description="Python SMPP Library",
-    long_description=open('README.rst', 'r').read(),
+    long_description=read_file('README.rst'),
     author='Praekelt Foundation',
     author_email='dev@praekeltfoundation.org',
     packages=find_packages(),
