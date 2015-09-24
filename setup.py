88e05dfe2a56f2b3f95309bcbef752a79e873143
Simon Cross <hodgestar@gmail.com>
2015-09-24 11:36:45 +0200

    Bump version.

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def read_file(filename):
 
 setup(
     name="python-smpp",
-    version="0.1.8",
+    version="0.1.9a",
     url='http://github.com/praekelt/python-smpp',
     license='BSD',
     description="Python SMPP Library",
